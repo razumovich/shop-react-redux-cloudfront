6d97908cc029d719be37b129ec56dee4ab3ba50e
aliaksandr.razumovich <aliaksandr_razumovich@epam.com>
2023-05-14 04:22:32 +0300

    Authorization

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://g9tw46pdk7.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://1jba1flmg1.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://krs2lilvy2.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://8rh7z5aa9b.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
